d596d3b68f45c7a798f40486e5f8e2d25fea4d4a
badock <jonathan.pastor@me.com>
2019-03-06 01:03:26 +0100

    simplification of the firmware : it now detects interferences

--- templates/inos/SensorTC.ino
+++ templates/inos/SensorTC.ino
@@ -196,28 +196,28 @@ float getCalibratedTemperature(int deviceIndex, float uncalibratedTemperature) {
 /*   Serial.println(" *Fahrenheit"); */
 /* } */
 
-float askTemperature(DeviceAddress devAddr) {
-  byte i;
-  byte present = 0;
-  byte temptype;
-  byte data[12];
-  float celsius, fahrenheit;
-  byte addr[8];
+/* float askTemperature(DeviceAddress devAddr) { */
+/*   byte i; */
+/*   byte present = 0; */
+/*   byte temptype; */
+/*   byte data[12]; */
+/*   float celsius, fahrenheit; */
+/*   byte addr[8]; */
 
-  for(int i = 0; i < 8; i++) {
-    addr[i] = devAddr[i];
-  }
+/*   for(int i = 0; i < 8; i++) { */
+/*     addr[i] = devAddr[i]; */
+/*   } */
 
-  if (OneWire::crc8(addr, 7) != addr[7]) {
-      return sqrt(-1);
-  }
+/*   if (OneWire::crc8(addr, 7) != addr[7]) { */
+/*       return sqrt(-1); */
+/*   } */
 
   
-  ds.reset();
-  ds.select(addr);
-  ds.write(0x44, 1);        // start conversion, with parasite power on at the end
+/*   ds.reset(); */
+/*   ds.select(addr); */
+/*   ds.write(0x44, 1);        // start conversion, with parasite power on at the end */
 
-  return -1;
+/*   return -1; */
 }
 
 /* float getTempC_raw(DeviceAddress devAddr) { */
@@ -271,6 +271,11 @@ float askTemperature(DeviceAddress devAddr) {
 /*   return celsius; */
 /* } */
 
+float distance(float a, float b) {
+  float difference = a-b;
+  return sqrt(difference * difference);
+}
+
 void setup(void) {
   Serial.begin(9600);
 
@@ -278,7 +283,7 @@ void setup(void) {
   
   scan_address_indexes();
   sensors.begin();
-
+  
   // report parasite power requirements
   Serial.print("Parasite power is: "); 
   if (sensors.isParasitePowerMode()) Serial.println("ON");
@@ -286,36 +291,44 @@ void setup(void) {
   
   // Initialize the WIFI:
   WiFi.begin(ssid, password);
+  /* WiFi.setOutputPower(0); */
+  /* WiFi.forceSleepBegin(); */
 }
 
 int cpt = 0;
+float temperatures_array[16][3];
+int temperatures_array_initialization_step = 0;
+
 void loop(void) {
   Serial.println("Start of cycle");
-  if (cpt == 0) {    
+  if (cpt == 0) {
     for (int i =0; i< 16; i++) {
         DeviceAddress* devAddrPtr = addresses[i];
         String macAddressAsString = deviceAddressToString(*devAddrPtr);
         Serial.println(" Sensor "+String(i)+" = "+macAddressAsString);
     }
   }
-  
-  /* Serial.println("Asking temperatures"); */
-  
-  /* for (int deviceIndex = 0; deviceIndex < 16; deviceIndex++) { */
-  /*   DeviceAddress* devAddrPtr = addresses[deviceIndex]; */
-  /*   askTemperature(*devAddrPtr); */
-  /* } */
-  /* Serial.println(" -> Done asking temperatures"); */
-  /* delay(850); */
-  
+       
   sensors.begin();
   sensors.requestTemperatures();
+
+  /* // Prevent noise on the onewire bus */
+  /* delay(850); */
   
-  Serial.println("Iterating over sensors");
+  /* Serial.println("Iterating over sensors"); */
 
   String msg = "[";
   int count = 0;
-  for (int deviceIndex = 0; deviceIndex < 16; deviceIndex++) {
+  int indexes[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15};
+  for (int i=0; i < 16; i++) {
+     int n = random(0, 16);  // Integer from 0 to questionCount-1
+     int temp = indexes[n];
+     indexes[n] =  indexes[i];
+     indexes[i] = temp;
+  }
+  
+  for (int i = 0; i < 16; i++) {
+      int deviceIndex = indexes[i];
       DeviceAddress* devAddrPtr = addresses[deviceIndex];
       float temperature = sensors.getTempC(*devAddrPtr);    
       //float temperature = getTempC_raw(*devAddrPtr);
@@ -326,21 +339,60 @@ void loop(void) {
       String macAddressAsString = deviceAddressToString(*devAddrPtr);
 
       if (!isnan(temperature) && temperature > -10) {
-        Serial.println("["+String(deviceIndex)+"] --> "+ String(temperature)+" ("+String(avg_temperature)+")"+" ("+String(calibratedTemperature)+")");
+        /* Serial.println("["+String(deviceIndex)+"] --> "+ String(temperature)+" ("+String(avg_temperature)+")"+" ("+String(calibratedTemperature)+")"); */
         
         if (count > 0) {
           msg += ",";
         }
-        msg += "{\"sensor\":\""+String(macAddressAsString)+"\",\"t\":\"T\",\"v\":"+String(calibratedTemperature)+"}";
-        count += 1;
+        
+
+        if (temperatures_array_initialization_step > 2) {
+          temperatures_array[deviceIndex][0] = temperatures_array[deviceIndex][1];
+        }
+        if (temperatures_array_initialization_step > 1) {
+          temperatures_array[deviceIndex][1] = temperatures_array[deviceIndex][2];
+        }
+        temperatures_array[deviceIndex][2] = calibratedTemperature;
+
+        float a = temperatures_array[deviceIndex][0];
+        float b = temperatures_array[deviceIndex][1];
+        float c = temperatures_array[deviceIndex][2];
+
+        int skip_sending = 0;
+
+        if (distance(a,b) > 1.0 && distance(a,c) < 1.0) {
+          skip_sending = 1;
+        }
+
+        if (!skip_sending) {
+          String current_value = "{\"sensor\":\""+String(macAddressAsString)+"\",\"t\":\"T\",\"v\":"+String(b)+"}";
+          msg += current_value;
+          count += 1;
+        }
+
+
+        /* // Prevent noise on the OneWire bus */
+        /* if (deviceIndex > 10) { */
+        /*   delay(100); */
+        /* } else { */
+        /*   delay(50); */
+        /* } */
+        /* delay(50); */
       }
-      delay(100);
   }
   msg += "]";
 
   cpt = (cpt + 1) % 30;
 
-  Serial.println("I will send data to the temperature service");
+  /* Serial.println("Turning on wifi"); */
+  /* WiFi.setOutputPower(5); */
+  /* delay(1); */
+  /* // Bring up the WiFi connection */
+  /* WiFi.mode( WIFI_STA ); */
+  /* /\* WiFi.begin( WLAN_SSID, WLAN_PASSWD ); *\/ */
+  /* WiFi.begin(ssid, password); */
+
+  /* Serial.println("I will send data to the temperature service"); */
   HTTPClient http;
   //http.begin("http://192.168.1.50:8080/temperature/list");
   http.begin("http://10.1.0.4:8080/temperature/list");
@@ -349,12 +401,20 @@ void loop(void) {
   int httpCode = http.POST(msg);
   String payload = http.getString();
   http.end();
-  Serial.println("    - httpCode: "+String(httpCode));
-  Serial.println("    - payload: "+String(payload));
-  Serial.println("  -> Done sending data!");
+  /* Serial.println("    - httpCode: "+String(httpCode)); */
+  /* Serial.println("    - payload: "+String(payload)); */
+  /* Serial.println("  -> Done sending data!"); */
+  
+  /* Serial.println("Turning off wifi"); */
+  /* WiFi.setOutputPower(0); */
+  /* WiFi.forceSleepBegin(); */
+  
+  if (temperatures_array_initialization_step < 3) {
+    temperatures_array_initialization_step += 1;
+  }
 
   digitalWrite(LED_BUILTIN, LOW);
   delay(100);
   digitalWrite(LED_BUILTIN, HIGH);
-  Serial.println("End of cycle");
+  /* Serial.println("End of cycle"); */
 }
